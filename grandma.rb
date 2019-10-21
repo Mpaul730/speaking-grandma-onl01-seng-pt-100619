e3a4ff7d3d8ae56a9e6f332d9caee0cc9a1075b6
Mark Paul <mpaul730@gmail.com>
2019-10-21 16:50:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,7 +18,7 @@ def speak_to_grandma(talk)
     return "HUH?! SPEAK UP, SONNY!"
   elsif talk.upcase
     return "NO, NOT SINCE 1938!"
-    elsif talk = "I LOVE YOU GRANDMA!"
+    elsif talk == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
 end 
